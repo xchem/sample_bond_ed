191b709ae4e36a0850550fd2e40347c45cb4bb3c
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-20 12:19:14 +0100

    Intergrate along bond

--- src/sample_bond_ed/sample_bond_ed.py
+++ src/sample_bond_ed/sample_bond_ed.py
@@ -444,7 +444,7 @@ def intergrate_along_bond(
     )
     sample_centers = get_sample_centres(bond, num_sample_along_bond)
 
-    sample_positions = get_sample_positions_near_samples(sample_centers, 10000, 0.25)
+    sample_positions = get_sample_positions_near_samples(sample_centers, 10000, 0.5)
     print(sample_positions[0])
 
     # Get the predicted density
