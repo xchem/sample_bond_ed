765c437b044f5848e337079598ada70c45ca1ce2
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-20 13:16:52 +0100

    Intergrate along bond

--- src/sample_bond_ed/sample_bond_ed.py
+++ src/sample_bond_ed/sample_bond_ed.py
@@ -529,7 +529,7 @@ class CLI:
                 {
                     "Dataset": dataset,
                     "Chirality": "S",
-                    "Density": density
+                    "Mean Positive Electron Density": density
 
                 }
             )
